56a79b50be3a7dfbf822da91dedae503c10bae3a
angela chaj <angelachaj648@gmail.com>
2026-03-13 01:42:57 -0500

    usuario

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿static void SumaUsuario()
+{
+	Console.Write("Numero 1 ");
+	double a = Convert.ToDouble(Console.ReadLine());
+
+	Console.Write("Numero 2 ");
+	double b = Convert.ToDouble(Console.ReadLine());
+
+	Console.WriteLine("Suma: " + (a + b));
+}
